acb42b2b45160796e797fa9108953456881fe2ca
AleksandraVelikoseltceva <a19041997@gmail.com>
2023-11-06 13:42:08 +0300

    pp-1-2

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("Иван", "Иванов", (byte) 18);
+        userService.saveUser("Олег", "Олегов", (byte) 19);
+        userService.saveUser("Саша", "Александров", (byte) 20);
+        userService.saveUser("Вадим", "Вадимов", (byte) 21);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
+
