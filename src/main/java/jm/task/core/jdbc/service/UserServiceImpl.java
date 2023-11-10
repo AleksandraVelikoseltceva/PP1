8836a1895bf43d177544bbbba6a4ac68cfb91c3a
AleksandraVelikoseltceva <a19041997@gmail.com>
2023-11-10 17:46:54 +0300

    bug fix 2

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
@@ -12,19 +13,23 @@ public class UserServiceImpl implements UserService {
     public void createUsersTable() {
         userDao.createUsersTable();
     }
+
     @Override
     public void dropUsersTable() {
         userDao.dropUsersTable();
     }
+
     @Override
-    public void saveUser(String name, String lastName, byte age) {
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
         userDao.saveUser(name, lastName, age);
         System.out.println("User с именем – " + name + " добавлен в базу данных");
     }
+
     @Override
-    public void removeUserById(long id) {
+    public void removeUserById(long id) throws SQLException {
         userDao.removeUserById(id);
     }
+
     @Override
     public List<User> getAllUsers() {
         List<User> users = userDao.getAllUsers();
@@ -35,7 +40,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public void cleanUsersTable() {
+    public void cleanUsersTable() throws SQLException {
         userDao.cleanUsersTable();
     }
 }
